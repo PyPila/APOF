625aa0bd040083c995dd940f13a51ecab49f29af
Bartlomiej Wiater <bartlomiej.wiater@stxnext.pl>
2017-01-18 15:37:37 +0100

    added Restaurant model and migration

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    """Restaurant model"""
+    name = models.CharField(blank=False, max_length=50, unique=True)
+    logo = models.ImageField(blank=False, upload_to='logos/')
+    opening = models.CharField(
+        blank=False,
+        max_length=100,
+        verbose_name='Opening hours',
+    )
+
+    def __str__(self):
+        return self.name
+
