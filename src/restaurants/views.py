369c56954fcf69b642ddd4b9e63f6b133a67c7f0
Bartlomiej Wiater <bartlomiej.wiater@stxnext.pl>
2017-01-18 17:34:02 +0100

    added index template, view

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Restaurant
+
+
+def restaurants_list(request):
+    """View for restaurants list."""
+    restaurants = Restaurant.objects.all()
+    return render(request, 'restaurants/index.html', {'restaurants': restaurants})
