c59768fa6f2ebf02aa716381592607c8570d24d9
Bartlomiej Wiater <bartlomiej.wiater@stxnext.pl>
2017-01-19 14:00:53 +0100

    renamed url name

--- src/restaurants/urls.py
+++ src/restaurants/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import restaurants_list
 
 urlpatterns = [
-    url(r'^$', restaurants_list, name='index')
+    url(r'^$', restaurants_list, name='restaurant-list')
 ]
