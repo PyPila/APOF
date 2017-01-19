d8052eea4a5107690fac4abb73885e57173fcc4a
Bartlomiej Wiater <bartlomiej.wiater@stxnext.pl>
2017-01-19 14:11:56 +0100

    test for restaurant list view

--- src/restaurants/tests.py
+++ src/restaurants/tests.py
@@ -1,14 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Restaurant
 
 
-class TestRestaurant(TestCase):
-    """Tests from Restaurant model"""
+class RestaurantTestCase(TestCase):
+
     def test_string_representation(self):
-        """Tests restaurnat string representation equals it's name"""
         test_restaurant_name = 'Test restaurant name'
-        self.assertEquals(
+        self.assertEqual(
             str(Restaurant(name=test_restaurant_name)),
             test_restaurant_name
         )
+
+
+class RestaurantListTestCase(TestCase):
+
+    def test_main_page_view_is_restaurants_list_view(self):
+        self.assertEqual('/', reverse('restaurant-list'))
+
+    def test_restaurants_list_view(self):
+        response = self.client.get(reverse('restaurant-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'restaurants/index.html')
