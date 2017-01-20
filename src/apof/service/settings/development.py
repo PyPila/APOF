46eab3c51aa0dd3c688b136f7b4f2c7a8a7bc1ee
Radek Lejba <radek.lejba@stxnext.pl>
2017-01-20 11:28:23 +0100

    "Added menu template"

--- src/apof/service/settings/development.py
+++ src/apof/service/settings/development.py
@@ -3,6 +3,6 @@ from apof.service.settings.common import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 AUTH_PASSWORD_VALIDATORS = []
---
+++ src/apof/service/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+
+def root_page(request):
+    return HttpResponse('Root Home Page')
