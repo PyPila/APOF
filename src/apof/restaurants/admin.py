6488551ecb49cd27c5892fe4d28b03cd300060d3
Bartlomiej Wiater <bartlomiej.wiater@stxnext.pl>
2017-01-30 13:40:44 +0100

    added phone number to restaurants admin view

--- src/apof/restaurants/admin.py
+++ src/apof/restaurants/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 
-from .models import Restaurant, OpeningHours
+from .models import Restaurant, OpeningHours, PhoneNumber
 
 
 class OpeningHoursInlineAdmin(admin.TabularInline):
     model = OpeningHours
 
 
+class PhoneNumberInlineAdmin(admin.TabularInline):
+    model = PhoneNumber
+
+
 class RestaurantAdmin(admin.ModelAdmin):
     inlines = [
         OpeningHoursInlineAdmin,
+        PhoneNumberInlineAdmin
     ]
 
 
