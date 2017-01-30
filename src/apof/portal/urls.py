1aaa14318adc238ca30a7c247c01721d23e00bc0
Radek Lejba <radek.lejba@stxnext.pl>
2017-01-30 13:43:36 +0100

    Added google authentication

--- src/apof/portal/urls.py
+++ src/apof/portal/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
-
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'', include('social_django.urls', namespace='social')),
+    url(r'^$', views.index, name='home'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/portal'}, name='logout'),
+    url(r'^login/$', views.login, name='login'),
 ]
--- src/apof/portal/views.py
+++ src/apof/portal/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-from django.http import HttpResponse
 
-
+@login_required
 def index(request):
     return render(request, 'portal/index.html')
+
+
+def login(request):
+    return render(request, 'portal/login.html')
