3eb8fbdc9e7d7941629b8c10b6d886830df23feb
Radek Lejba <radek.lejba@stxnext.pl>
2017-01-31 12:19:02 +0100

    added some stylistic changes

--- src/apof/portal/urls.py
+++ src/apof/portal/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
     url(r'', include('social_django.urls', namespace='social')),
     url(r'^$', views.index, name='home'),
-    url(r'^login/$', auth_views.login, {'redirect_authenticated_user': True}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/portal'}, name='logout'),
+    url(r'^login/$',
+        auth_views.login,
+        {'redirect_authenticated_user': True},
+        name='login'),
+    url(r'^logout/$',
+        auth_views.logout,
+        {'next_page': '/portal'},
+        name='logout'),
 ]
