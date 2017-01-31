91528a8bf3502fcccb95a888fefc7eb74fc0107b
Bartlomiej Wiater <bartlomiej.wiater@stxnext.pl>
2017-01-31 15:54:00 +0100

    attempt to hover card

--- src/apof/restaurants/views.py
+++ src/apof/restaurants/views.py
@@ -1,8 +1,18 @@
+from datetime import datetime
+
+from django.db.models import Prefetch
 from django.shortcuts import render
 
-from .models import Restaurant
+from .models import Restaurant, OpeningHours
 
 
 def restaurants_list(request):
-    restaurants = Restaurant.objects.all()
+    restaurants = Restaurant.objects.all().prefetch_related('phonenumber_set')
+    restaurants = restaurants.prefetch_related(
+        Prefetch(
+            'openinghours_set',
+            queryset=OpeningHours.objects.filter(day=datetime.today().weekday()),
+            to_attr='opening_hours'
+        )
+    )
     return render(request, 'restaurants/index.html', {'restaurants': restaurants})
