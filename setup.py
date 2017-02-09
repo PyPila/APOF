3b5ed8096d76d799c16a766a0f01c2ae3093f306
Radek Lejba <radek.lejba@stxnext.pl>
2017-02-09 13:14:27 +0100

    Sorted install_requies list in alphabetical order

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 install_requires = [
     'django>=1.10',
     'mysql-python==1.2.5',
-    'social-auth-app-django==1.0.1',
     'pillow==4.0.0',
+    'social-auth-app-django==1.0.1',
 ]
 develop_requires = [
     'pdbpp',
