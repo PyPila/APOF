df431848c4f40646ed527616c8b4e7f5e721b8c5
Bartlomiej Wiater <bartlomiej.wiater@stxnext.pl>
2017-02-13 12:49:00 +0100

    basket and order models

--- src/apof/baskets/admin.py
+++ src/apof/baskets/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Basket, Order
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('status', 'basket', 'meal')
+
+
+class BasketAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Basket, BasketAdmin)
+admin.site.register(Order, OrderAdmin)
--- src/apof/baskets/models.py
+++ src/apof/baskets/models.py
@@ -1,5 +1,55 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from menus.models import Meal, Size, Topping
+
+
+ORDER_STATUS = (
+    (0, 'Waiting'),
+    (1, 'Canceled'),
+    (2, 'Ordered'),
+    (3, 'Delivered')
+)
+
+
+class Basket(models.Model):
+    owner = models.OneToOneField(
+        User,
+        blank=False,
+        null=True,
+        on_delete=models.PROTECT,
+        unique=True
+    )
+
+    def __unicode__(self):
+        return '{} {}'.format(self.__class__.__name__, self.owner.username)
+
+    def __repr_(self):
+        return '{} (Owner: {})'.format(self.__class_.__name__, self.owner)
+
+
+class Order(models.Model):
+    status = models.IntegerField(choices=ORDER_STATUS, default=0)
+    meal = models.ForeignKey(Meal, on_delete=models.PROTECT)
+    size = models.ForeignKey(Size, on_delete=models.PROTECT)
+    toppings = models.ManyToManyField(Topping, blank=True)
+    quantity = models.IntegerField(blank=True, default=1)
+    basket = models.ForeignKey(Basket, blank=False, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __unicode__(self):
+        return '{} {} {}'.format(self.basket.owner, self.meal, self.created_at)
+
+    def __repr_(self):
+        return '{} ( Basket {}, Meal: {}, Created at: {})'.format(
+            self.__class__.__name__,
+            self.basket,
+            self.meal,
+            self.created_at
+        )
