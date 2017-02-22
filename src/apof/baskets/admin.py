f5388c57298f775bd7f079aaaa77ccf5635c138a
Bartlomiej Wiater <bartlomiej.wiater@stxnext.pl>
2017-02-22 14:30:20 +0100

    removed custom admin list view

--- src/apof/baskets/admin.py
+++ src/apof/baskets/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.admin.views.main import ChangeList
 
 from restaurants.models import Restaurant
 from .models import Basket, Order
@@ -10,28 +9,13 @@ class RestaurantListFilter(admin.SimpleListFilter):
     parameter_name = 'restaurant'
 
     def lookups(self, request, model_admin):
-        list_tuple = []
-        for restaurant in Restaurant.objects.all():
-            list_tuple.append((restaurant.id, restaurant.name))
-        return list_tuple
+        return Restaurant.objects.values_list('id', 'name')
 
     def queryset(self, request, queryset):
         if self.value():
             return queryset.filter(meal__menu__restaurant__id=self.value())
         else:
             return queryset
-
-
-class TotalChangeList(ChangeList):
-
-    def get_results(self, request, *args, **kwargs):
-        super(TotalChangeList, self).get_results(request, *args, **kwargs)
-        self.prices_sum = sum(r.get_total_price() for r in self.result_list)
-
-        restaurant_id = request.GET.get('restaurant')
-        self.phone_numbers = []
-        if restaurant_id:
-            self.phone_numbers = Restaurant.objects.get(pk=restaurant_id).get_phone_numbers()
 
 
 class OrderAdmin(admin.ModelAdmin):
@@ -46,11 +30,6 @@ class OrderAdmin(admin.ModelAdmin):
         'get_order_price'
     )
     list_filter = ('basket__created_at', 'status', RestaurantListFilter)
-
-    change_list_template = 'admin/order/change_list.html'
-
-    def get_changelist(self, request):
-        return TotalChangeList
 
     def get_meal_name(self, obj):
         return obj.meal.name
