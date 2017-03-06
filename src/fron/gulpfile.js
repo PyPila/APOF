a1d5fb4e5d3fa50358624b91dd754cfbf3e09ad8
Radek Lejba <radek.lejba@stxnext.pl>
2017-03-06 14:38:47 +0100

    Removed comments from gulpfile.js, Implemented nesting into sass files

--- src/fron/gulpfile.js
+++ src/fron/gulpfile.js
@@ -1,6 +1,3 @@
-// ///////////////////////////////////////////////////////////////////
-// Required
-// ///////////////////////////////////////////////////////////////////
 var browserSync = require('browser-sync').create(),
     cleanCSS = require('gulp-clean-css'),
     gulp = require('gulp'),
@@ -11,9 +8,6 @@ var browserSync = require('browser-sync').create(),
     uglify = require('gulp-uglify');
 
 
-// ///////////////////////////////////////////////////////////////////
-// PATHS
-// ///////////////////////////////////////////////////////////////////
 const statics = {
     portal: {
         static: '../apof/portal/static/portal/',
@@ -39,13 +33,11 @@ const paths = {
     deps: {
         portal: {
             js: statics.portal.static + 'js/*.js',
-            min_js: statics.portal.static + 'js/*.min.js',
             scss: statics.portal.static + 'scss/*.scss',
             html: statics.portal.templates + '*.html'
         },
         restaurants: {
             js: statics.restaurants.static + 'js/*.js',
-            min_js: statics.restaurants.static + 'js/*.min.js',
             scss: statics.restaurants.static + 'scss/*.scss',
             html: statics.restaurants.templates + '*.html'
         }
@@ -53,56 +45,50 @@ const paths = {
 }
 
 
-// ///////////////////////////////////////////////////////////////////
-// Sass tasks
-// ///////////////////////////////////////////////////////////////////
 gulp.task('sass', function(){
     var portalPath = gulp.src(paths.deps.portal.scss)
-        .pipe(plumber())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest(paths.dist.portal.css))
-        .pipe(browserSync.stream()),
+            .pipe(plumber())
+            .pipe(sass().on('error', sass.logError))
+            .pipe(cleanCSS())
+            .pipe(gulp.dest(paths.dist.portal.css))
+            .pipe(browserSync.stream()),
 
         restaurantsPath = gulp.src(paths.deps.restaurants.scss)
-        .pipe(plumber())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest(paths.dist.restaurants.css))
-        .pipe(browserSync.stream());
+            .pipe(plumber())
+            .pipe(sass().on('error', sass.logError))
+            .pipe(cleanCSS())
+            .pipe(gulp.dest(paths.dist.restaurants.css))
+            .pipe(browserSync.stream());
 
     return merge(portalPath, restaurantsPath)
 });
 
 
-// ///////////////////////////////////////////////////////////////////
-// JS Tasks
-// ///////////////////////////////////////////////////////////////////
 gulp.task('scripts', function(){
-    var portalPath = gulp.src([paths.deps.portal.js, '!' + paths.deps.portal.min_js])
-        .pipe(plumber())
-        .pipe(rename({suffix:'.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.dist.portal.js))
-        .pipe(browserSync.stream()),
+    var portalPath = gulp.src([
+                paths.deps.portal.js,
+                '!' + paths.dist.portal.js + '*.min.js'
+            ])
+            .pipe(plumber())
+            .pipe(rename({suffix:'.min'}))
+            .pipe(uglify())
+            .pipe(gulp.dest(paths.dist.portal.js))
+            .pipe(browserSync.stream()),
 
         restaurantsPath = gulp.src([
-            paths.deps.restaurants.js,
-            '!' + paths.deps.restaurants.min_js
-        ])
-        .pipe(plumber())
-        .pipe(rename({suffix:'.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.dist.restaurants.js))
-        .pipe(browserSync.stream());
+                paths.deps.restaurants.js,
+                '!' + paths.dist.portal.js + '*.min.js'
+            ])
+            .pipe(plumber())
+            .pipe(rename({suffix:'.min'}))
+            .pipe(uglify())
+            .pipe(gulp.dest(paths.dist.restaurants.js))
+            .pipe(browserSync.stream());
 
     return merge(portalPath, restaurantsPath)
 });
 
 
-// ///////////////////////////////////////////////////////////////////
-// Browser-Sync Tasks
-// ///////////////////////////////////////////////////////////////////
 gulp.task('browser-sync', function() {
     browserSync.init({
         proxy: {
@@ -113,18 +99,12 @@ gulp.task('browser-sync', function() {
 });
 
 
-// ///////////////////////////////////////////////////////////////////
-// HTML Tasks
-// ///////////////////////////////////////////////////////////////////
 gulp.task('html', function(){
     gulp.src('../**/*.html')
-    .pipe(browserSync.stream());
+        .pipe(browserSync.stream());
 });
 
 
-// ///////////////////////////////////////////////////////////////////
-// Watch tasks
-// ///////////////////////////////////////////////////////////////////
 gulp.task('sass:watch', function(){
     gulp.watch(
         [paths.deps.portal.scss, paths.deps.restaurants.scss],
@@ -135,13 +115,10 @@ gulp.task('sass:watch', function(){
         ['html']
     );
     gulp.watch(
-    [paths.deps.portal.js, paths.deps.restaurants.js],
-    ['scripts']
+        [paths.deps.portal.js, paths.deps.restaurants.js],
+        ['scripts']
     );
 });
 
 
-// ///////////////////////////////////////////////////////////////////
-// Default tasks
-// ///////////////////////////////////////////////////////////////////
 gulp.task('default', ['sass', 'sass:watch', 'html', 'browser-sync', 'scripts']);
