202fc8421628f0abf73133583be9fcc3ced418cf
wiaterb <wiaterb@gmail.com>
2017-03-23 18:45:30 +0100

    registered models in admin panel without providing a ModelAdmin

--- src/apof/menus/admin.py
+++ src/apof/menus/admin.py
@@ -32,31 +32,15 @@ class ToppingAdmin(admin.ModelAdmin):
     list_filter = ('menu', )
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    pass
-
-
-class PriceAdmin(admin.ModelAdmin):
-    pass
-
-
-class IngredientAdmin(admin.ModelAdmin):
-    pass
-
-
-class MenuAdmin(admin.ModelAdmin):
-    pass
-
-
 class SizeAdmin(admin.ModelAdmin):
     list_display = ('description', 'value', 'value_unit', 'menu')
     list_filter = ('menu', )
 
 
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Category)
+admin.site.register(Ingredient)
 admin.site.register(Meal, MealAdmin)
-admin.site.register(Menu, MenuAdmin)
-admin.site.register(Price, PriceAdmin)
+admin.site.register(Menu)
+admin.site.register(Price)
 admin.site.register(Size, SizeAdmin)
 admin.site.register(Topping, ToppingAdmin)
