a54252f67410c40f79cfbc922ffec79629087c37
Radek Lejba <radek.lejba@stxnext.pl>
2017-03-23 12:13:11 +0100

    Fixed issues with ID's

--- fron/src/js/index.js
+++ fron/src/js/index.js
@@ -1,16 +1,16 @@
-$('#menu-toggle').click(function(event) {
+$('.menu-toggle').click(function(event) {
     event.preventDefault();
     $('.row').toggleClass('toggled');
 });
 
 $(document).ready(function () {
-    var topNav = $('.navbar fixed-top');
+    var topNav = $('nav.fixed-top.navbar-light');
 
     $(window).scroll(function() {
         if ($(this).scrollTop() > 50) {
-            $('#top-nav').addClass('fades');
+            $(topNav).addClass('fades');
         } else {
-            $('#top-nav').removeClass('fades');
+            $(topNav).removeClass('fades');
         }
     });
 });
